b376836e6ef67cc375cf6b9cb1b538b7f78bc726
Dan Kolbman <dankolbman@gmail.com>
2018-12-19 21:38:58 -0500

    :sparkles: Add speed to state

---
+++ src/reducers/SpeedReducer.js
@@ -0,0 +1,38 @@
+import { combineReducers } from 'redux';
+
+const pageError = (state = {'error': false, 'message': ''}, action = {}) => {
+  switch(action.type) {
+    case 'REQUEST_TRIP_SPEED_PAGE_ERROR':
+      return {
+        error: action.hasError,
+        message: action.message,
+      }
+    default:
+      return state;
+  }
+}
+
+const pageLoading = (state = false, action = {}) => {
+  switch(action.type) {
+    case 'REQUEST_TRIP_SPEED_PAGE_LOADING':
+      return action.loading;
+    default:
+      return state;
+  }
+}
+
+const pageSuccess = (state = [], action = {}) => {
+  switch(action.type) {
+    case 'REQUEST_TRIP_SPEED_PAGE_SUCCESS':
+      return action.points;
+    default:
+      return state;
+  }
+}
+
+
+export default combineReducers({
+  points: pageSuccess,
+  pageError,
+  pageLoading,
+});
