3a8c4a03926bfb6e8f29e65f6fb88d506e9507a4
Dan Kolbman <dan@kolbman.com>
2022-01-29 13:47:01 -0800

    :fire: Here we go again

--- src/client.js
+++
@@ -1,8 +0,0 @@
-import ApolloClient from 'apollo-boost';
-
-console.log(process.env.REACT_APP_API);
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_API,
-});
-
-export default client;
