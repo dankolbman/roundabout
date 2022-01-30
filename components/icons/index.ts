dde933f4d6159b25c4037bf32e7ed3ade7d53be1
Dan Kolbman <dan@kolbman.com>
2022-01-29 20:53:45 -0800

    :art: Add adventure icons

---
+++ components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as AutoIcon } from "./auto";
+export { default as BicycleIcon } from "./bicycle";
+export { default as MotorcycleIcon } from "./motorcycle";
+export { default as ScooterIcon } from "./scooter";
