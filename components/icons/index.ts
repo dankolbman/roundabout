6011b404c3dabed333a81f2bdc03afe15c04369e
Dan Kolbman <dan@kolbman.com>
2022-03-13 22:27:12 -0700

    :rocket: Deploy gh-pages

--- components/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AutoIcon } from "./auto";
-export { default as BicycleIcon } from "./bicycle";
-export { default as MotorcycleIcon } from "./motorcycle";
-export { default as ScooterIcon } from "./scooter";
